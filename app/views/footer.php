0f40625f91fe5afc83dc7a9736d31c987ec6b520
Daniel Brendel <dbrendel1988@gmail.com>
2022-05-02 02:45:02 +0200

    Twitter link in footer if set

--- app/views/footer.php
+++ app/views/footer.php
@@ -6,6 +6,10 @@
             <div class="footer-frame">
                 <div class="footer-content">
                     &copy; {{ date('Y') }} by {{ env('APP_NAME') }} | <a class="link-light-dark" href="{{ url('/imprint') }}">Imprint</a>&nbsp;&bull;&nbsp;<a class="link-light-dark" href="{{ url('/privacy') }}">Privacy policy</a> | Visits: {{ $view_count }}
+
+                    @if (env('APP_TWITTERFEED') !== null)
+                        | <span class="is-pointer" title="Twitter" onclick="window.open('https://twitter.com/{{ env('APP_TWITTERFEED') }}');"><i class="fab fa-twitter"></i></span>
+                    @endif
                 </div>
             </div>
         </div>
