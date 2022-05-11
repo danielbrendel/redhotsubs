8e0550fcb1b87fef7ec115a4c6e78e4e0152aba1
Daniel Brendel <dbrendel1988@gmail.com>
2022-05-12 01:43:50 +0200

    Typo

--- app/views/layout.php
+++ app/views/layout.php
@@ -2,7 +2,7 @@
 <html lang="{{ getLocale() }}">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-with, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<meta name="author" content="{{ env('APP_AUTHOR') }}">
 		<meta name="description" content="{{ env('APP_DESCRIPTION') }}">
