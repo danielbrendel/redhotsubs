84802370f2dab07204d80e1fbd10b677e0961225
Daniel Brendel <dbrendel1988@gmail.com>
2022-07-15 00:50:03 +0200

    Formatting

--- app/migrations/TwitterHistoryModel.php
+++ app/migrations/TwitterHistoryModel.php
@@ -1,53 +1,50 @@
 <?php
 
-    /*
-        Asatru PHP - Migration for twitterhistory
-    */
+/**
+ * Class TwitterHistoryModel_Migration
+ */
+class TwitterHistoryModel_Migration
+{
+    private $database = null;
+    private $connection = null;
 
     /**
-     * This class specifies a migration
+     * Store the PDO connection handle
+     * 
+     * @param \PDO $pdo The PDO connection handle
+     * @return void
      */
-    class TwitterHistoryModel_Migration {
-        private $database = null;
-        private $connection = null;
-
-        /**
-         * Store the PDO connection handle
-         * 
-         * @param \PDO $pdo The PDO connection handle
-         * @return void
-         */
-        public function __construct($pdo)
-        {
-            $this->connection = $pdo;
-        }
-
-        /**
-         * Called when the table shall be created or modified
-         * 
-         * @return void
-         */
-        public function up()
-        {
-            $this->database = new Asatru\Database\Migration('twitterhistory', $this->connection);
-            $this->database->drop();
-            $this->database->add('id INT NOT NULL AUTO_INCREMENT PRIMARY KEY');
-            $this->database->add('ident VARCHAR(512) NOT NULL');
-            $this->database->add('sub VARCHAR(512) NOT NULL');
-            $this->database->add('title VARCHAR(512) NOT NULL');
-            $this->database->add('permalink VARCHAR(512) NOT NULL');
-            $this->database->add('created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
-            $this->database->create();
-        }
-
-        /**
-         * Called when the table shall be dropped
-         * 
-         * @return void
-         */
-        public function down()
-        {
-            if ($this->database)
-                $this->database->drop();
-        }
+    public function __construct($pdo)
+    {
+        $this->connection = $pdo;
     }
+
+    /**
+     * Called when the table shall be created or modified
+     * 
+     * @return void
+     */
+    public function up()
+    {
+        $this->database = new Asatru\Database\Migration('twitterhistory', $this->connection);
+        $this->database->drop();
+        $this->database->add('id INT NOT NULL AUTO_INCREMENT PRIMARY KEY');
+        $this->database->add('ident VARCHAR(512) NOT NULL');
+        $this->database->add('sub VARCHAR(512) NOT NULL');
+        $this->database->add('title VARCHAR(512) NOT NULL');
+        $this->database->add('permalink VARCHAR(512) NOT NULL');
+        $this->database->add('created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
+        $this->database->create();
+    }
+
+    /**
+     * Called when the table shall be dropped
+     * 
+     * @return void
+     */
+    public function down()
+    {
+        if ($this->database)
+            $this->database->drop();
+    }
+}
