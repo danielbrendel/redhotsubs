c524f93ba33c9c8877dbe952ed1ea7f158d001c4
Daniel Brendel <dbrendel1988@gmail.com>
2022-08-30 00:02:41 +0200

    Update deps

--- app/views/video.php
+++ app/views/video.php
@@ -12,7 +12,7 @@
             <a id="view-post" href=""></a>
         </div>
 
-        <div class="video-swiper" id="video-swiper" ontouchstart="window.touchStartX = event.changedTouches[0].screenX;" ontouchend="window.touchEndX = event.changedTouches[0].screenX; if ((window.touchStartX < window.touchEndX) || (window.touchStartX > window.touchEndX)) { window.vue.fetchNextVideo('video-content', 'view-post'); }"></div>
+        <div class="video-swiper" id="video-swiper" ontouchstart="window.touchStartX = event.changedTouches[0].screenX; window.touchStartY = event.changedTouches[0].screenY;" ontouchend="window.touchEndX = event.changedTouches[0].screenX; window.touchEndY = event.changedTouches[0].screenY; if (((window.touchStartX < window.touchEndX) || (window.touchStartX > window.touchEndX)) && (Math.abs(window.touchEndY - window.touchStartY) < 32)) { window.vue.fetchNextVideo('video-content', 'view-post'); }"></div>
         <div class="video-content" id="video-content"><center><i class="fas fa-spinner fa-spin"></i></center></div>
 
         <div class="video-controls">
