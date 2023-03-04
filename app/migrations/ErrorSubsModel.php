f5cccea291ac2ef9ba7ecd5e4fed69844f07327d
Daniel Brendel <dbrendel1988@gmail.com>
2023-03-04 22:53:03 +0100

    Format

--- app/migrations/ErrorSubsModel.php
+++ app/migrations/ErrorSubsModel.php
@@ -1,52 +1,49 @@
 <?php
 
-    /*
-        Asatru PHP - Migration for errorsubs
-    */
+/**
+ * Class ErrorSubsModel_Migration
+ */
+class ErrorSubsModel_Migration
+{
+    private $database = null;
+    private $connection = null;
 
     /**
-     * This class specifies a migration
+     * Store the PDO connection handle
+     * 
+     * @param \PDO $pdo The PDO connection handle
+     * @return void
      */
-    class ErrorSubsModel_Migration {
-        private $database = null;
-        private $connection = null;
-
-        /**
-         * Store the PDO connection handle
-         * 
-         * @param \PDO $pdo The PDO connection handle
-         * @return void
-         */
-        public function __construct($pdo)
-        {
-            $this->connection = $pdo;
-        }
-
-        /**
-         * Called when the table shall be created or modified
-         * 
-         * @return void
-         */
-        public function up()
-        {
-            $this->database = new Asatru\Database\Migration('errorsubs', $this->connection);
-            $this->database->drop();
-            $this->database->add('id INT NOT NULL AUTO_INCREMENT PRIMARY KEY');
-            $this->database->add('subname VARCHAR(512) NOT NULL');
-            $this->database->add('error INT NOT NULL');
-            $this->database->add('reason VARCHAR(512) NULL');
-            $this->database->add('created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
-            $this->database->create();
-        }
-
-        /**
-         * Called when the table shall be dropped
-         * 
-         * @return void
-         */
-        public function down()
-        {
-            if ($this->database)
-                $this->database->drop();
-        }
+    public function __construct($pdo)
+    {
+        $this->connection = $pdo;
     }
+
+    /**
+     * Called when the table shall be created or modified
+     * 
+     * @return void
+     */
+    public function up()
+    {
+        $this->database = new Asatru\Database\Migration('errorsubs', $this->connection);
+        $this->database->drop();
+        $this->database->add('id INT NOT NULL AUTO_INCREMENT PRIMARY KEY');
+        $this->database->add('subname VARCHAR(512) NOT NULL');
+        $this->database->add('error INT NOT NULL');
+        $this->database->add('reason VARCHAR(512) NULL');
+        $this->database->add('created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
+        $this->database->create();
+    }
+
+    /**
+     * Called when the table shall be dropped
+     * 
+     * @return void
+     */
+    public function down()
+    {
+        if ($this->database)
+            $this->database->drop();
+    }
+}
