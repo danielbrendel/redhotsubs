7d743a97dbaf392f4354ba5c17783bdb6c06914c
Daniel Brendel <dbrendel1988@gmail.com>
2024-02-01 00:50:02 +0100

    Upgrade to current framework

--- app/models/AppSettingsModel.php
+++ app/models/AppSettingsModel.php
@@ -12,7 +12,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getImprint()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('imprint');
         } catch (Exception $e) {
             throw $e;
@@ -26,7 +26,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getPrivacyPolicy()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('privacy');
         } catch (Exception $e) {
             throw $e;
@@ -40,7 +40,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getAbout()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('about');
         } catch (Exception $e) {
             throw $e;
@@ -54,7 +54,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getAgeConsent()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('age_consent');
         } catch (Exception $e) {
             throw $e;
@@ -68,7 +68,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getAppContent()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('app');
         } catch (Exception $e) {
             throw $e;
@@ -82,7 +82,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getHeadCode()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('head_code');
         } catch (Exception $e) {
             throw $e;
@@ -109,7 +109,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getInfo()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             return $item->get('info');
         } catch (Exception $e) {
             throw $e;
@@ -123,7 +123,7 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getInfoStyle()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             $value = $item->get('info_style');
 
             if ((!is_string($value)) || (strlen($value) === 0)) {
@@ -143,10 +143,24 @@ class AppSettingsModel extends \Asatru\Database\Model
     public static function getCategories()
     {
         try {
-            $item = AppSettingsModel::where('id', '=', 1)->first();
+            $item = AppSettingsModel::getSettings();
             $cats = $item->get('categories');
             return explode(',', $cats);
         } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @param $item
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function getSettings($item = 1)
+    {
+        try {
+            return AppSettingsModel::raw('SELECT * FROM `' . self::tableName() . '` WHERE id = ?', [$item])->first();
+        } catch (\Exception $e) {
             throw $e;
         }
     }
